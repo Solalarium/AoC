7a2da9145afcc9fef5cbbea1dea767e048df2ab1
Patrick Hoheisel <Patrick@Patricks-MacBook-Pro.local>
2019-12-03 21:04:12 +0100

    Boom

---
+++ util.py
@@ -0,0 +1,52 @@
+class Day:
+    def __init__(self, day: int, part: int):
+        from problems import description
+        self.day  = day
+        self.part = part
+        self.desc = description(day, part)
+    
+    def load(self, typing=str, sep="\n") -> list:
+        """Loads Data for Problem
+        File _must_ be named dayXX.txt
+        Returns data and makes it available as attribte "data"
+        Keyword Arguments:
+            typing {[type]} -- Type of data in list (default: {str})
+        
+        Returns:
+            list -- Data for Problem
+        """
+        with open(f"day{self.day:02d}.txt") as f:
+            data = f.read().split(sep)
+        if "" in data:
+            data.remove("")
+        self.data = list(map(typing, data))
+        self.raw_data = self.data.copy()
+        return self.data
+    
+    def reset(self):
+        """Reset Data to original
+        """
+        self.data = self.raw_data.copy()
+
+    def apply(self, func) -> list:
+        """Apply a function to every element.
+        Changes the original data.
+        Arguments:
+            func {function} -- Function to apply to every element in input
+        
+        Returns:
+            list -- Function applied to every element in input
+        """
+        self.data = list(map(func, self.data))
+        return self.data
+
+    def answer(self, num) -> str:
+        return f"The Solution on Day {self.day} for Part {self.part} is: {num}"
+
+if __name__ == "__main__":
+    day = Day(1,1)
+
+    print("Data is:", day.data(int))
+    print("Day is:", day.day)
+    print("Part is:", day.part)
+    print("Description is:", day.desc)
