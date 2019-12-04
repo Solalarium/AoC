29ca67cbda8f35af52f13a0ab2c8271081bb24b2
Patrick Hoheisel <Patrick@Patricks-MacBook-Pro.local>
2019-12-04 19:12:04 +0100

    "Optimization"

--- day04.py
+++ day04.py
@@ -1,9 +1,5 @@
 from util import Day
 
-def split(x):
-    if x >= 1000000 or x <= 99999: return(False)
-    return([x//100000, (x % 100000)//10000, (x % 10000)//1000, (x % 1000)//100, (x % 100)//10, x % 10])
-
 def is_pass_v1(x):
     if x == False: return(False)
     if x[0] <= x[1] <= x[2] <= x[3] <= x[4] <= x[5]:
@@ -12,7 +8,8 @@ def is_pass_v1(x):
     return(False)
 
 def is_pass(x):
-    x = split(x)
+    x = str(x)
+    if len(x) != 6: return False
     if x == False: return(False)
     if x[0] <= x[1] <= x[2] <= x[3] <= x[4] <= x[5]:
         if x[0] == x[1] != x[2] or x[0] != x[1] == x[2] != x[3] or x[1] != x[2] == x[3] != x[4] or x[2] != x[3] == x[4] != x[5] or x[3] != x[4] == x[5]:
@@ -22,8 +19,8 @@ def is_pass(x):
 def foo(lower,upper):
     count = 0
     for i in range(lower,upper+1):
-        if is_pass(i): count += 1     
-    return(count)      
+        if is_pass(i): count += 1
+    return(count)
 
 day04 = Day(4,2)
 day04.load(int,sep='-')
